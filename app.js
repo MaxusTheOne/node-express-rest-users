3d4ff364aa7937022d8ea32e715c3ac71a49ac67
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-09-14 13:53:18 +0200

    mysql exercise

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import connection from "./backend.js";
 import express from "express";
 import fs from "fs/promises";
 import cors from "cors";
@@ -9,69 +10,82 @@ app.use(express.json()); // To parse JSON bodies
 app.use(cors()); // Enable CORS for all routes
 
 app.get("/", (request, response) => {
-    response.send("Node.js Users REST API 🎉");
+  response.send("Node.js Users REST API 🎉");
 });
 
 async function getUsersFromJSON() {
-    const data = await fs.readFile("data.json");
-    const users = JSON.parse(data);
-    users.sort((userA, userB) => userA.name.localeCompare(userB.name));
-    return users;
+  const data = await fs.readFile("data.json");
+  const users = JSON.parse(data);
+  users.sort((userA, userB) => userA.name.localeCompare(userB.name));
+  return users;
 }
 
 // READ all users
-app.get("/users", async (request, response) => {
-    response.json(await getUsersFromJSON());
+app.get("/users", (request, response) => {
+  const query = "SELECT * FROM users ORDER BY name;";
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results);
+    }
+  });
 });
 
 // READ one user
 app.get("/users/:id", async (request, response) => {
-    const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
-    const users = await getUsersFromJSON();
-    const user = users.find(user => user.id === id);
-    response.json(user);
+  const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const query = "SELECT * FROM users WHERE id=?";
+  const values = [id];
+  connection.query(query, values, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      response.json(results[0]);
+    }
+  });
 });
 
 // CREATE user
 app.post("/users", async (request, response) => {
-    const newUser = request.body;
-    newUser.id = new Date().getTime();
-    console.log(newUser);
+  const newUser = request.body;
+  newUser.id = new Date().getTime();
+  console.log(newUser);
 
-    const users = await getUsersFromJSON();
-    users.push(newUser);
-    fs.writeFile("data.json", JSON.stringify(users));
-    response.json(users);
+  const users = await getUsersFromJSON();
+  users.push(newUser);
+  fs.writeFile("data.json", JSON.stringify(users));
+  response.json(users);
 });
 
 // UPDATE user
 app.put("/users/:id", async (request, response) => {
-    const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
-    const users = await getUsersFromJSON();
-    let userToUpdate = users.find(user => user.id === id);
-    const body = request.body;
-    userToUpdate.image = body.image;
-    userToUpdate.mail = body.mail;
-    userToUpdate.name = body.name;
-    userToUpdate.title = body.title;
+  const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const users = await getUsersFromJSON();
+  let userToUpdate = users.find(user => user.id === id);
+  const body = request.body;
+  userToUpdate.image = body.image;
+  userToUpdate.mail = body.mail;
+  userToUpdate.name = body.name;
+  userToUpdate.title = body.title;
 
-    fs.writeFile("data.json", JSON.stringify(users));
-    response.json(users);
+  fs.writeFile("data.json", JSON.stringify(users));
+  response.json(users);
 });
 
 // DELETE user
 app.delete("/users/:id", async (request, response) => {
-    const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
-    const users = await getUsersFromJSON();
-    // const newUsers = users.filter(user => user.id !== id);
-    const index = users.findIndex(user => user.id === id);
-    users.splice(index, 1);
-    fs.writeFile("data.json", JSON.stringify(users));
-    response.json(users);
+  const id = request.params.id; // tager id fra url'en, så det kan anvendes til at finde den givne bruger med "det" id.
+  const users = await getUsersFromJSON();
+  // const newUsers = users.filter(user => user.id !== id);
+  const index = users.findIndex(user => user.id === id);
+  users.splice(index, 1);
+  fs.writeFile("data.json", JSON.stringify(users));
+  response.json(users);
 });
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-    console.log(`App listening on http://localhost:${port}`);
-    console.log(`Users Endpoint http://localhost:${port}/users`);
+  console.log(`App listening on port ${port}`);
+  console.log(`App listening on http://localhost:${port}`);
+  console.log(`Users Endpoint http://localhost:${port}/users`);
 });
